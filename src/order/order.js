79270de11f5681ab336b41d53f1eb2861a44bdc2
AyaNS2003 <aya.sawalha.2003@gmail.com>
2025-04-05 23:30:14 +0300

    feat: saving orders into json file

--- src/order/order.js
+++ src/order/order.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-
+const fs = require('fs')
 var app = express()
 
 var port = 3000
@@ -20,22 +20,53 @@ app.post("/purchase/:id",async (req,res)=>{
         }
 
         console.log('Book fetched successfully to order service')
+        var newQuantity = bookData.quantity
+        var bookPrice = bookData.price
+        var state
+        var response
 
         if(bookData.quantity > 0){
-            var newQuantity = bookData.quantity - 1
-            var bookPrice = bookData.price
-
+            newQuantity = bookData.quantity - 1
+            state = "succeeded"
+            
             const update = await axios.post('http://localhost:4000/update/' + book_id, 
                 {quantity: newQuantity,
                 price: bookPrice})//update book qty
 
-            return res.status(200).json({ message: 'Book purchased successfully' });
+            response = res.status(200).json({ message: 'Book purchased successfully' });
 
         }//book can be purchased
         else{
-            return res.status(400).json({ message: 'Book out of stock' });
-        }//qty is 0 or less
+            state = "failed"
+            response = res.status(400).json({ message: 'Book out of stock' });
+        }//qty is 0 
+
+        var order = {
+            "title": bookData.title,
+            "newQuantity": newQuantity,
+            "price": bookPrice,
+            "state": state,
+        }
         
+        //adding order to file
+        var orders = [];
+        fs.readFile('src/order/orders.json', 'utf8', (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                orders = JSON.parse(data);
+                orders.push(order);
+                fs.writeFile('src/order/orders.json', JSON.stringify(orders,null,2), function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Order added to file successfully!');
+                    }
+                });
+            }
+        });
+        return response
     }
     catch(err){
         console.error(err.message);
