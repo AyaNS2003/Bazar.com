9eb34c7145e6d14448a01c7ea3ba2402f4c7a728
AyaNS2003 <aya.sawalha.2003@gmail.com>
2025-04-05 21:45:23 +0300

    feat: implementing purchasing in order service

--- src/order/order.js
+++ src/order/order.js
@@ -11,14 +11,34 @@ app.post("/purchase/:id",async (req,res)=>{
     var book_id = req.params.id
     
     try{
-        const response = await axios.get('http://catalog:4000/info/' + book_id) 
-        var responseData = response.data;
-        console.log('Book fetched successfully to order service');
-        console.log(responseData);
+        const qry = await axios.get('http://localhost:4000/info/' + book_id) //fetching book
+        
+        var bookData= qry.data;
+        if(!bookData) {// book not found
+            console.log('Book not found in catalog service');
+            return res.status(404).json({ message: 'Book not found' });
+        }
+
+        console.log('Book fetched successfully to order service')
+
+        if(bookData.quantity > 0){
+            var newQuantity = bookData.quantity - 1
+            var bookPrice = bookData.price
+
+            const update = await axios.post('http://localhost:4000/update/' + book_id, 
+                {quantity: newQuantity,
+                price: bookPrice})//update book qty
+
+            return res.status(200).json({ message: 'Book purchased successfully' });
+
+        }//book can be purchased
+        else{
+            return res.status(400).json({ message: 'Book out of stock' });
+        }//qty is 0 or less
         
     }
     catch(err){
-        console.error('Error fetching data:', err.message);
+        console.error(err.message);
         res.status(500).json({ message: 'Internal server error' });
     }   
 })
